69ef07d7b53acdd977ce5205e0945c3f944b388e
mmakini19 <mmakini19@gmail.com>
2023-02-21 17:27:36 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/*
+ *  main - print a line of code using puts
+ *  Retun:0
+ */
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0)
+}
